2be48d936e59917544d3829971b8b5b1ee0790f7
Nick Brassel <nick@tzarc.org>
2023-03-18 21:26:01 +1100

    Sizing.

--- squid-container/support/entrypoint.sh
+++ squid-container/support/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-sed -i -e "s/@@ALLOWED_DOMAINS_LIST@@/${ALLOWED_DOMAINS_LIST}/g" ${SQUID_CONFIG_FILE}
-sed -i -e "s/@@CACHE_SIZE@@/${CACHE_SIZE:-256}/g" ${SQUID_CONFIG_FILE}
+sed -i \
+    -e "s/@@ALLOWED_DOMAINS_LIST@@/${ALLOWED_DOMAINS_LIST}/g" \
+    -e "s/@@MEM_CACHE_SIZE@@/${MEM_CACHE_SIZE:-256}/g" \
+    -e "s/@@FILE_CACHE_SIZE@@/${FILE_CACHE_SIZE:-1024}/g" \
+    -e "s/@@FILE_MAX_SIZE@@/${FILE_MAX_SIZE:-100}/g" \
+    ${SQUID_CONFIG_FILE}
 /usr/sbin/squid -f ${SQUID_CONFIG_FILE} --foreground -z && exec /usr/sbin/squid -f ${SQUID_CONFIG_FILE} --foreground -YCd 1
